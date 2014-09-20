6123426ceb22a7cabd93650a9aac4ec1606a8940
A-learn <hayama710145@gmail.com>
2014-09-20 19:12:17 +0800

    12

--- Server/code/WifiSignal.java
+++ Server/code/WifiSignal.java
@@ -28,7 +28,7 @@ public class WifiSignal extends Thread{
 					in = new BufferedReader(new 
 								InputStreamReader(wificlient.getInputStream()));
 					String str = in.readLine();
-						
+						//1
 					FileWriter fw = new FileWriter("C://CamTest//Positioning.txt");
 					synchronized(fw){
 						fw.write(str+"");
